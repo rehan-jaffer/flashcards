18c120f3438ff9680462d2d5d19fee77e2efb251
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-28 16:39:59 +0100

    added some rudimentary tests, need to decouple this from activerecord behaviour

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Deck, type: :model do
 
   it { should have_many(:cards) }
 
+  describe "instance methods" do
+
+    before(:all) do
+      @deck = Deck.create!(name: "Test Deck")
+      @card = Card.create!(front: "some", back: "thing", deck_id: @deck.id)
+    end
+
+    it "returns the size of the deck correctly" do
+      expect(@deck.size).to eq 1
+    end
+
+    it "increases of a card is added" do
+      Card.create(front: "some", back: "thing", deck_id: @deck.id)
+      expect(@deck.size).to eq 2
+    end
+
+    it "decreases if a card is removed" do
+      @card.destroy
+      expect(@deck.size).to eq 0
+    end
+
+  end
+
 end
